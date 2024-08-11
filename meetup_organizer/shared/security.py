0439f2770efe6d1e6556c6bad86d1185ca1b8313
Bento Luiz <bentoluizv@gmail.com>
2024-08-10 22:34:18 -0300

    add hashing and verify hashed passwords functions

--- meetup_organizer/shared/security.py
+++ meetup_organizer/shared/security.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 
 from jwt import encode
+from pwdlib import PasswordHash
 from zoneinfo import ZoneInfo
 
 from meetup_organizer.shared.settings import Settings
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = PasswordHash.recommended()
 
 
 def create_access_token(data: dict):
@@ -22,3 +25,11 @@ def create_access_token(data: dict):
         algorithm=Settings().ALGORITHM,  # type: ignore
     )
     return encoded_jwt
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
