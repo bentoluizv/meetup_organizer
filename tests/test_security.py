5f601c7d2684623adf06853e00f72aa8a9462192
Bento Luiz <bentoluizv@gmail.com>
2024-08-10 23:52:35 -0300

    add get current user with token test

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,6 +1,9 @@
 from jwt import decode
 
-from meetup_organizer.shared.security import create_access_token
+from meetup_organizer.shared.security import (
+    create_access_token,
+    get_current_user,
+)
 from meetup_organizer.shared.settings import Settings
 
 
@@ -9,3 +12,8 @@ def test_generate_jwt():
     decoded_token = decode(token, Settings().SECRET_KEY, Settings().ALGORITHM)  # type: ignore
     assert decoded_token['sub'] == 'test'
     assert decoded_token['exp']
+
+
+def test_get_current_user(session, token):
+    user = get_current_user(session, token)
+    assert user.username == 'test'
